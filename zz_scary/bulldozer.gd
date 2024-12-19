27458601fdc4220ce578533c942fc6749ac95a76
upbeatDiamond <upbeatDiamond@gmail.com>
2024-12-19 02:14:33 -0500

    Update bulldozer.gd

--- zz_scary/bulldozer.gd
+++ zz_scary/bulldozer.gd
@@ -1,14 +1,72 @@
 extends Node
 
-enum CSState {
-	BLANK,
-	KEYWORD,
-	SPACE,
-	STRING,
-	INTEGER,
+enum CSToken {
+	ABSTRACT,
+	AS,
+	BASE,
+	BOOL,
+	BREAK,
+	BYTE,
+	CASE,
+	CATCH,
+	CHAR,
+	CHECKED,
+	CLASS,
+	CONST,
+	CONTINUE,
+	DECIMAL,
+	DEFAULT,
+	DELEGATE,
+	DO,
+	DOUBLE,
+	ELSE,
+	ENUM,
+	EVENT,
+	EXPLICIT,
+	EXTERN,
+	FALSE,
+	FINALLY,
+	FIXED,
 	FLOAT,
+	FOR,
+	FOREACH,
+	GOTO,
+	IF,
+	IMPLICIT,
+	IN,
+	INT,
+	INTERFACE,
+}
+
+enum CSState {
+	BLANK, ## The initial state
+	KEYWORD, ## Reserved words, or where they ought to be
+	CLASS_NAME, ## Declaration of class name
+	SPACE, ## Dummy state; used to cache/reset the symbol, and query next state
+	STRING, ## While in a string, the only way out is an unescaped "
+	STRING_ESCAPE, ## An escape character
+	INTERPOLATED_STRING, ## Looks like $"...{x}", and can be replaced with str("...", x)
+	INTEGER, ## A number with no fancy suffixes
+	DECIMAL, ## A number with a decimal separator detected
+	FLOAT, ## A DECIMAL followed by 'f'
+	ARGUMENT, ## When waiting for type or 'out'
+	ARGUMENT_TYPE, ## After an 'out', looks for type
+	PARAMETER,
 	END_OF_LINE,
 }
+
+const KEYWORDS := ["abstract", "as", "base", "bool", "break", "byte", "case", 
+		"catch", "char", "checked", "class", "const", "continue", "decimal",
+		"default", "delegate", "do", "double", "else", "enum", "event",
+		"explicit", "extern", "false", "finally", "fixed", "float", "for",
+		"foreach", "goto", "if", "implicit", "in", "int", "interface", 
+		"internal", "is", "lock", "long", "namespace", "new", "null", "while",
+		"object", "operator", "out", "override", "params", "private",
+		"protected", "public", "readonly", "ref", "return", "sbyte", "sealed",
+		"short", "sizeof", "stackalloc", "static", "string", "struct",
+		"switch", "this", "throw", "true", "try", "typeof", "uint", "ulong",
+		"unchecked", "unsafe", "ushort", "using", "virtual", "void", "volatile",
+]
 
 var scope := 1
 var state := CSState.BLANK
